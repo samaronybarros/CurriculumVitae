f1cada6413541925252039ad45abdb8afe448e5d
Sam <samarony.barros@gmail.com>
2018-05-15 13:45:36 -0300

    Criado tabelas do banco de dados

--- app/src/main/java/com/example/sam/curriculumvitae/database/DadosOpenHelper.java
+++ app/src/main/java/com/example/sam/curriculumvitae/database/DadosOpenHelper.java
@@ -7,19 +7,32 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DadosOpenHelper extends SQLiteOpenHelper {
 
     public DadosOpenHelper(Context context) {
-        super(context, "Dados", null, 2);
+        super(context, "Dados", null, 3);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(ScriptDDL.createTableInfoPessoais());
         db.execSQL(ScriptDDL.createTableObjetivo());
+        db.execSQL(ScriptDDL.createTableFormacao());
+        db.execSQL(ScriptDDL.createTableExperiencia());
+        db.execSQL(ScriptDDL.createTableCurso());
+        db.execSQL(ScriptDDL.createTableQualificacao());
+        db.execSQL(ScriptDDL.createTableIdioma());
+        db.execSQL(ScriptDDL.createTableApresentacao());
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(ScriptDDL.dropTableInfoPessoais());
         db.execSQL(ScriptDDL.dropTableObjetivo());
+        db.execSQL(ScriptDDL.dropTableFormacao());
+        db.execSQL(ScriptDDL.dropTableExperiencia());
+        db.execSQL(ScriptDDL.dropTableCurso());
+        db.execSQL(ScriptDDL.dropTableQualificacao());
+        db.execSQL(ScriptDDL.dropTableIdioma());
+        db.execSQL(ScriptDDL.dropTableApresentacao());
+
         onCreate(db);
     }
 }
--- app/src/main/java/com/example/sam/curriculumvitae/database/ScriptDDL.java
+++ app/src/main/java/com/example/sam/curriculumvitae/database/ScriptDDL.java
@@ -47,4 +47,134 @@ public class ScriptDDL {
 
         return sql.toString();
     }
+
+    public static String createTableFormacao() {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append(" CREATE TABLE IF NOT EXISTS FORMACAO( ");
+        sql.append("    CODIGO           INTEGER         PRIMARY KEY AUTOINCREMENT NOT NULL,");
+        sql.append("    CURSO            VARCHAR(250)    NOT NULL DEFAULT(''), ");
+        sql.append("    INSTITUICAO      VARCHAR(255)    NOT NULL DEFAULT(''), ");
+        sql.append("    STATUS           VARCHAR(20)     NOT NULL DEFAULT(''), ");
+        sql.append("    DATA_INICIO      VARCHAR(10)     NOT NULL DEFAULT(''), ");
+        sql.append("    DATA_CONCLUSAO   VARCHAR(10)     NOT NULL DEFAULT('') ");
+        sql.append(" ) ");
+
+        return sql.toString();
+    }
+
+    public static String dropTableFormacao() {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append(" DROP TABLE IF EXISTS FORMACAO; ");
+
+        return sql.toString();
+    }
+
+    public static String createTableExperiencia() {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append(" CREATE TABLE IF NOT EXISTS EXPERIENCIA( ");
+        sql.append("    CODIGO           INTEGER         PRIMARY KEY AUTOINCREMENT NOT NULL,");
+        sql.append("    EMPRESA          VARCHAR(50)     NOT NULL DEFAULT(''), ");
+        sql.append("    CARGO            VARCHAR(50)     NOT NULL DEFAULT(''), ");
+        sql.append("    ATIVIDADES       VARCHAR(255)    NOT NULL DEFAULT(''), ");
+        sql.append("    DATA_INICIO      VARCHAR(10)     NOT NULL DEFAULT(''), ");
+        sql.append("    DATA_FIM         VARCHAR(10)     NOT NULL DEFAULT(''), ");
+        sql.append("    TRABALHO_ATUAL   VARCHAR(03)     NOT NULL DEFAULT('') ");
+        sql.append(" ) ");
+
+        return sql.toString();
+    }
+
+    public static String dropTableExperiencia() {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append(" DROP TABLE IF EXISTS EXPERIENCIA; ");
+
+        return sql.toString();
+    }
+
+    public static String createTableCurso() {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append(" CREATE TABLE IF NOT EXISTS CURSO( ");
+        sql.append("    CODIGO           INTEGER         PRIMARY KEY AUTOINCREMENT NOT NULL,");
+        sql.append("    CURSO            VARCHAR(250)    NOT NULL DEFAULT(''), ");
+        sql.append("    INSTITUICAO      VARCHAR(255)    NOT NULL DEFAULT(''), ");
+        sql.append("    STATUS           VARCHAR(20)     NOT NULL DEFAULT(''), ");
+        sql.append("    DATA_INICIO      VARCHAR(10)     NOT NULL DEFAULT(''), ");
+        sql.append("    DATA_CONCLUSAO   VARCHAR(10)     NOT NULL DEFAULT(''), ");
+        sql.append("    DESCRICAO        VARCHAR(10)     NOT NULL DEFAULT('')  ");
+        sql.append(" ) ");
+
+        return sql.toString();
+    }
+
+    public static String dropTableCurso() {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append(" DROP TABLE IF EXISTS CURSO; ");
+
+        return sql.toString();
+    }
+
+    public static String createTableQualificacao() { //Qualificações e Atividades Profissionais
+        StringBuilder sql = new StringBuilder();
+
+        sql.append(" CREATE TABLE IF NOT EXISTS QUALIFICACAO( ");
+        sql.append("    CODIGO           INTEGER         PRIMARY KEY AUTOINCREMENT NOT NULL,");
+        sql.append("    ATIVIDADE        VARCHAR(250)    NOT NULL DEFAULT(''), ");
+        sql.append("    DESCRICAO        VARCHAR(10)     NOT NULL DEFAULT('')  ");
+        sql.append(" ) ");
+
+        return sql.toString();
+    }
+
+    public static String dropTableQualificacao() {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append(" DROP TABLE IF EXISTS QUALIFICACAO; ");
+
+        return sql.toString();
+    }
+
+    public static String createTableIdioma() { //Qualificações e Atividades Profissionais
+        StringBuilder sql = new StringBuilder();
+
+        sql.append(" CREATE TABLE IF NOT EXISTS IDIOMA( ");
+        sql.append("    CODIGO       INTEGER         PRIMARY KEY AUTOINCREMENT NOT NULL,");
+        sql.append("    IDIOMA       VARCHAR(10)     NOT NULL DEFAULT(''), ");
+        sql.append("    NIVEL        VARCHAR(10)     NOT NULL DEFAULT('')  ");
+        sql.append(" ) ");
+
+        return sql.toString();
+    }
+
+    public static String dropTableIdioma() {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append(" DROP TABLE IF EXISTS IDIOMA; ");
+
+        return sql.toString();
+    }
+
+    public static String createTableApresentacao() {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append(" CREATE TABLE IF NOT EXISTS APRESENTACAO( ");
+        sql.append("    CODIGO               INTEGER         PRIMARY KEY AUTOINCREMENT NOT NULL,");
+        sql.append("    DESCRICAO            VARCHAR(250)    NOT NULL DEFAULT('') ");
+        sql.append(" ) ");
+
+        return sql.toString();
+    }
+
+    public static String dropTableApresentacao() {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append(" DROP TABLE IF EXISTS APRESENTACAO; ");
+
+        return sql.toString();
+    }
 }
