c5e4c40b91c29268d82395401daa87878e98f7e2
Sam <samarony.barros@gmail.com>
2018-05-21 17:20:30 -0300

    ReModelada Tela Inicial

--- app/src/main/java/com/example/sam/curriculumvitae/activity/ActTelaInicial.java
+++ app/src/main/java/com/example/sam/curriculumvitae/activity/ActTelaInicial.java
@@ -2,54 +2,105 @@ package com.example.sam.curriculumvitae.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.example.sam.curriculumvitae.R;
 
-public class ActTelaInicial extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_info_pessoais:
-                    //mTextMessage.setText(R.string.title_info_pessoais);
-                    goToScreenInfo();
-                    return true;
-                case R.id.navigation_objetivo:
-                    //mTextMessage.setText(R.string.title_objetivo);
-                    goToScreenObjetivo();
-                    return true;
-                case R.id.navigation_formacao:
-                    //mTextMessage.setText(R.string.title_formacao);
-                    goToScreenFormacao();
-                    return true;
-                case R.id.navigation_experiencia:
-                    //mTextMessage.setText(R.string.title_experiencia);
-                    goToScreenExperiencia();
-                    return true;
-            }
-            return false;
-        }
-    };
+public class ActTelaInicial extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_tela_inicial);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.act_tela_inicial, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case (R.id.nav_info_pessoais):
+                goToScreenInfo();
+                break;
+            case (R.id.nav_objetivo):
+                goToScreenObjetivo();
+                break;
+            case (R.id.nav_formacao):
+                goToScreenFormacao();
+                break;
+            case (R.id.nav_experiencia):
+                goToScreenExperiencia();
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     public void goToScreenInfo() {
@@ -71,4 +122,5 @@ public class ActTelaInicial extends AppCompatActivity {
         Intent intent = new Intent(ActTelaInicial.this, ActExperiencia.class);
         startActivity(intent);
     }
+
 }
